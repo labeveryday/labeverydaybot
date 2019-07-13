5e4350164510997fa148d3b8091409dd56d5d9c7
Script Engineer <29664932+labeveryday@users.noreply.github.com>
2019-07-13 09:02:18 -0500

    Update tool.py

--- tool.py
+++ tool.py
@@ -22,67 +22,67 @@ print('The user is %s' % user.name)
 FILE_NAME = 'last_seen_id.txt'
 SECOND_FILE = 'last_reply'
 
-reply1 = ("Hey thanks for the message! Here's an #inspirationalquote.\n"
+reply1 = (" Hey thanks for the message! Here's an #inspirationalquote.\n"
           "'You miss 100% of the shots you don’t take. –Wayne Gretzky' #LabEveryday")
 
-reply2 = ("Whats up? Thanks for the message! Here's one of my favorite #inspirationalquotes.\n"
+reply2 = (" Whats up? Thanks for the message! Here's one of my favorite #inspirationalquotes.\n"
           "The mind is everything. What you think you become.  –Buddha' #LabEveryday")
 
-reply3 = ("Hola! Thanks for the message! Here's one of my favorite #inspirationalquotes.\n"
+reply3 = (" Hola! Thanks for the message! Here's one of my favorite #inspirationalquotes.\n"
           "The best time to plant a tree was 20 years ago. The second best time is now. –Chinese Proverb' #LabEveryday")
 
-reply4 = ("Whats good? Thanks for the message! Here's a #inspirationalquote.\n"
+reply4 = (" Whats good? Thanks for the message! Here's a #inspirationalquote.\n"
           "Eighty percent of success is showing up. –Woody Allen' #LabEveryday")
 
-reply5 = ("Hey thanks for the message! Here's a interesting #techfact.\n"
+reply5 = (" Hey thanks for the message! Here's a interesting #techfact.\n"
           "The first Apple logo was of Sir Isaac Newton sitting underneath a tree,\n"
           "with an apple about to hit his head.' #LabEveryday")
 
-reply6 = ("Hey! Thanks for the message! Here's a fun #techfact.\n"
+reply6 = (" Hey! Thanks for the message! Here's a fun #techfact.\n"
           "Bill Gates was a college dropout. And so was Kanye West!' #LabEveryday")
 
-reply7 = ("Hola! Thanks for the message! Here's a fun #techfact.\n"
+reply7 = (" Hola! Thanks for the message! Here's a fun #techfact.\n"
           "Did you know Alaska is the only state that can be typed on"
           " one row of a traditional QWERTY keyboard.' #LabEveryday")
 
-reply8 = ("Hey! Thanks for the message! Here's a #inspirationalquote.\n"
+reply8 = (" Hey! Thanks for the message! Here's a #inspirationalquote.\n"
           "Go confidently in the direction of your dreams.  "
           "Live the life you have imagined. –Henry David Thoreau' #LabEveryday")
 
-reply9 = ("Hey, how are you? Thanks for the message! Here's one awesome #inspirationalquote.\n"
+reply9 = (" Hey, how are you? Thanks for the message! Here's one awesome #inspirationalquote.\n"
           "Everything you’ve ever wanted is on the other side of fear. –George Addair' #LabEveryday")
 
-reply10 = ("Happy Monday! Wait, is it Monday? Thanks for the message! Here's one awesome #inspirationalquote.\n"
+reply10 = (" Happy Monday! Wait, is it Monday? Thanks for the message! Here's one awesome #inspirationalquote.\n"
            "You can’t fall if you don’t climb.  But there’s no joy in living your whole life on the ground.' "
            "#LabEveryday")
 
-reply11 = ("Turn up Tuesday! Ummm... Is it Tuesday? Thanks for the message! Here's a #inspirationalquote.\n"
+reply11 = (" Turn up Tuesday! Ummm... Is it Tuesday? Thanks for the message! Here's a #inspirationalquote.\n"
            "To learn and not do is really not to learn! - Stephan Covey' #LabEveryday")
 
 reply12 = (" love Fridays! Thanks for the message! Here's a truly #inspirationalquote.\n"
            "'It does not matter how slowly you go as long as you do not stop. –Confucius' #LabEveryday")
 
-reply13 = ("Hey whats up? Thanks for the message! Here's a super #techfact.\n"
+reply13 = (" Hey whats up? Thanks for the message! Here's a super #techfact.\n"
            "'Did you know 8 billion devices will be connected to the internet by 2020?\n"
            "Great time to be a NetworkEngineer' #LabEveryday")
 
-reply14 = ("Sunday Funday? I'm sure its not Sunday lol. Thanks for the message!\n"
+reply14 = (" Sunday Funday? I'm sure its not Sunday lol. Thanks for the message!\n"
            "Here's a interesting #techfact. Did you know that the company Cisco's name derived from San Francisco?'\n"
            "#LabEveryday")
 
-reply15 = ("Sunday Funday? I'm sure its not Sunday lol. Thanks for the message! Here's a interesting #techfact.\n"
+reply15 = (" Sunday Funday? I'm sure its not Sunday lol. Thanks for the message! Here's a interesting #techfact.\n"
            "Did you know that the company Cisco's name derived from San Francisco?' #LabEveryday")
 
-reply16 = ("Whats up fam? Thanks for the message! Here's a helpful #techfact.\n"
+reply16 = (" Whats up fam? Thanks for the message! Here's a helpful #techfact.\n"
            "Last time I checked. Indeed listed 60,827 open Network Engineer positions in the US!' #LabEveryday")
 
-reply17 = ("Hola! Thanks for the message! Here's a fun #techfact.\n"
+reply17 = (" Hola! Thanks for the message! Here's a fun #techfact.\n"
            "'Did you know Google receives over 63,000 searches per second on any given day?' #LabEveryday")
 
-reply18 = ("Hola! Thanks for the message! Here's a interesting #techfact.\n"
+reply18 = (" Hola! Thanks for the message! Here's a interesting #techfact.\n"
            "According to CBTNuggets there are an estimated 59,737 CCIEs in the world!' #LabEveryday")
 
-reply19 = ("What's! What day is it? If today is Wednesday I think I ran out of responses.......\n"
+reply19 = (" What's! What day is it? If today is Wednesday I think I ran out of responses.......\n"
            ".\n"
            ".\n"
            ".\n"
@@ -97,7 +97,7 @@ reply19 = ("What's! What day is it? If today is Wednesday I think I ran out of r
            "Here's a fun #techfact.\n"
            "'Technophobia is the fear of technology!' #LabEveryday")
 
-reply20 = ("What's! What day is it? If today is Thursday I am taking the day off.......\n"
+reply20 = (" What's! What day is it? If today is Thursday I am taking the day off.......\n"
            ".\n"
            ".\n"
            ".\n"
@@ -112,7 +112,7 @@ reply20 = ("What's! What day is it? If today is Thursday I am taking the day off
            "Here's a fun #techfact.\n"
            "'Did you know the last /22 block of ipv4 addresses were chopped and dished out and 2017?' #LabEveryday")
 
-reply21 = ("#Linux #QuizTime! Thanks for the message!\n"
+reply21 = (" #Linux #QuizTime! Thanks for the message!\n"
            "Here's a Linux related #techquestion.\n"
            "Which of these is not related to linux?\n"
            "A. Open platform\n"
@@ -122,7 +122,7 @@ reply21 = ("#Linux #QuizTime! Thanks for the message!\n"
            "#LabEveryday")
 
 
-reply22 = ("#Linux #QuizTime! Thanks for the message!\n"
+reply22 = (" #Linux #QuizTime! Thanks for the message!\n"
            "Here's a Linux related #techquestion.\n"
            "In Ubuntu how do execute Super User Privileges?\n"
            "A. sudo\n"
@@ -131,7 +131,7 @@ reply22 = ("#Linux #QuizTime! Thanks for the message!\n"
            "D. ssh -l\n"
            "#LabEveryday")
 
-reply23 = ("#Linux #QuizTime! Thanks for the message!\n"
+reply23 = (" #Linux #QuizTime! Thanks for the message!\n"
            "Here's a Linux related #techquestion.\n"
            "How do you navigate back to the home directory?\n"
            "A. cd ..\n"
@@ -140,14 +140,14 @@ reply23 = ("#Linux #QuizTime! Thanks for the message!\n"
            "D. cd\n"
            "#LabEveryday")
 
-reply24 = ("#Python #QuizTime! Thanks for the message!\n"
+reply24 = (" #Python #QuizTime! Thanks for the message!\n"
            "Here's a Python related #techquestion.\n"
            "Python is a interpreted language?\n"
            "A. True\n"
            "B. False\n"
            "#LabEveryday")
 
-reply25 = ("#Linux #QuizTime! Thanks for the message!\n"
+reply25 = (" #Linux #QuizTime! Thanks for the message!\n"
            "Here's a Linux related #techquestion.\n"
            "Which command is an improvement on the more command?\n"
            "A. cat\n"
@@ -156,7 +156,7 @@ reply25 = ("#Linux #QuizTime! Thanks for the message!\n"
            "D. tail\n"
            "#LabEveryday")
 
-reply26 = ("#Linux #QuizTime! Thanks for the message!\n"
+reply26 = (" #Linux #QuizTime! Thanks for the message!\n"
            "Here's a Linux related #techquestion.\n"
            "In which file is DNS information referenced?\n"
            "A. /etc/network/interfaces\n"
@@ -165,7 +165,7 @@ reply26 = ("#Linux #QuizTime! Thanks for the message!\n"
            "D. resolv.conf\n"
            "#LabEveryday")
 
-reply27 = ("#Linux #QuizTime! Thanks for the message!\n"
+reply27 = (" #Linux #QuizTime! Thanks for the message!\n"
            "Here's a Linux related #techquestion.\n"
            "How do you display interface ip information in linux?\n"
            "A. ipconfig\n"
@@ -174,7 +174,7 @@ reply27 = ("#Linux #QuizTime! Thanks for the message!\n"
            "D. show ip route"
            "#LabEveryday")
 
-reply28 = ("#Linux #QuizTime! Thanks for the message!\n"
+reply28 = (" #Linux #QuizTime! Thanks for the message!\n"
            "Here's a Linux related #techquestion.\n"
            "What is the name of the first process that the Linux kernel runs, aside from itself?\n"
            "A. who\n"
@@ -183,7 +183,7 @@ reply28 = ("#Linux #QuizTime! Thanks for the message!\n"
            "D. bootp\n"
            "#LabEveryday")
 
-reply29 = ("#Linux #QuizTime! Thanks for the message!\n"
+reply29 = (" #Linux #QuizTime! Thanks for the message!\n"
            "Here's a Linux related #techquestion.\n"
            "Which command changes a files group?\n"
            "A. groupmod\n"
@@ -192,7 +192,7 @@ reply29 = ("#Linux #QuizTime! Thanks for the message!\n"
            "D. chown\n"
            "#LabEveryday")
 
-reply30 = ("#Security #QuizTime! Thanks for the message!\n"
+reply30 = (" #Security #QuizTime! Thanks for the message!\n"
            "Here's a Security+ related #techquestion.\n"
            "What would you use to verify security controls are in place?\n"
            "A. ping\n"
@@ -201,7 +201,7 @@ reply30 = ("#Security #QuizTime! Thanks for the message!\n"
            "D. vulnerability scan\n"
            "#LabEveryday")
 
-reply31 = ("#Security #QuizTime! Thanks for the message!\n"
+reply31 = (" #Security #QuizTime! Thanks for the message!\n"
            "Here's a Security+ related #techquestion.\n"
            "On which port would you apply an ACL to block IMAP traffic?\n"
            "A. 143\n"
@@ -210,7 +210,7 @@ reply31 = ("#Security #QuizTime! Thanks for the message!\n"
            "D. 3389\n"
            "#LabEveryday")
 
-reply32 = ("#Security #QuizTime! Thanks for the message!\n"
+reply32 = (" #Security #QuizTime! Thanks for the message!\n"
            "Here's a Security+ related #techquestion.\n"
            "What would you use to verify security controls are in place?\n"
            "A. ping\n"
@@ -219,7 +219,7 @@ reply32 = ("#Security #QuizTime! Thanks for the message!\n"
            "D. vulnerability scan\n"
            "#LabEveryday")
 
-reply33 = ("#Security #QuizTime! Thanks for the message!\n"
+reply33 = (" #Security #QuizTime! Thanks for the message!\n"
            "Here's a Security+ related #techquestion.\n"
            "Which protocols utilize UDP ports 161 and 162?\n"
            "A. HTTPS\n"
@@ -228,7 +228,7 @@ reply33 = ("#Security #QuizTime! Thanks for the message!\n"
            "D. APPLETALK\n"
            "#LabEveryday")
 
-reply34 = ("#Security #QuizTime! Thanks for the message!\n"
+reply34 = (" #Security #QuizTime! Thanks for the message!\n"
            "Here's a Security+ related #techquestion.\n"
            "Full disk encryption prevents?\n"
            "A. Client side attacks\n"
@@ -246,7 +246,7 @@ reply35 = ("#Security #QuizTime! Thanks for the message!\n"
            "D. Pivilege escalation\n"
            "#LabEveryday")
 
-reply36 = ("#Security #QuizTime! Thanks for the message!\n"
+reply36 = (" #Security #QuizTime! Thanks for the message!\n"
            "Here's a Security+ related #techquestion.\n"
            "Which of the following sets multiple flag fields in a TCP packet?\n"
            "A. ARP poisoning\n"
@@ -264,7 +264,7 @@ reply37 = ("#Security #QuizTime! Thanks for the message!\n"
            "D. Key to the city\n"
            "#LabEveryday")
 
-reply38 = ("#Security #QuizTime! Thanks for the message!\n"
+reply38 = (" #Security #QuizTime! Thanks for the message!\n"
            "Here's a Security+ related #techquestion.\n"
            "This is used to trick people in social engineer?\n"
            "A. Software scan\n"
@@ -273,7 +273,7 @@ reply38 = ("#Security #QuizTime! Thanks for the message!\n"
            "D. Applications issues\n"
            "#LabEveryday")
 
-reply39 = ("#Security #QuizTime! Thanks for the message!\n"
+reply39 = (" #Security #QuizTime! Thanks for the message!\n"
            "Here's a Security+ related #techquestion.\n"
            "Which is related to Bitlocker?\n"
            "A. ping\n"
@@ -282,7 +282,7 @@ reply39 = ("#Security #QuizTime! Thanks for the message!\n"
            "D. ipconfig\n"
            "#LabEveryday")
 
-reply40 = ("#CCNA #QuizTime! Thanks for the message!\n"
+reply40 = (" #CCNA #QuizTime! Thanks for the message!\n"
            "Here's a CCNA related #techquestion.\n"
            "A MAC address operates at what layer of the OSI model?\n"
            "A. 1\n"
@@ -291,7 +291,7 @@ reply40 = ("#CCNA #QuizTime! Thanks for the message!\n"
            "D. 7\n"
            "#LabEveryday")
 
-reply41 = ("#CCNA #QuizTime! Thanks for the message!\n"
+reply41 = (" #CCNA #QuizTime! Thanks for the message!\n"
            "Here's a CCNA related #techquestion.\n"
            "Which MAC address begins a frame?\n"
            "A. destination node\n"
@@ -305,7 +305,7 @@ reply_list = [reply1, reply2, reply3, reply4, reply5, reply6, reply7, reply8, re
               reply22, reply23, reply24, reply25, reply26, reply27, reply28, reply29, reply30, reply31, reply32,
               reply33, reply34, reply35, reply36, reply37, reply38, reply39, reply40, reply41]
 
-general_reply = "Hi thanks for the message! I am just a BOT that will soon have super powers! #StayTuned"
+general_reply = " Hi thanks for the message! I am just a BOT that will soon have super powers! #StayTuned"
 
 
 def find_tweets():
